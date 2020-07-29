5b9ea74228054f6f1208d9101d8a0c7aeba91706
Brian Speight <brspeight@expediagroup.com>
2020-07-29 08:31:36 -0700

    Components for a common left nav and page-specific right nav

---
+++ src/components/page-nav.js
@@ -0,0 +1,30 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+const PgNavContainer = styled.section`
+  padding: 2rem;
+  font-size: 1.4rem;
+  margin: -1.5rem 0 0 2rem;
+  width: 25rem;
+  flex-shrink: 0;
+  h3 {
+    text-transform: uppercase;
+  }
+  ul {
+    padding: 0;
+    li {
+      list-style-type: none;
+    }
+  }
+`
+
+const PageNav = () => {
+  return (
+    <PgNavContainer>
+      <h3>On this page</h3>
+    </PgNavContainer>
+  )
+}
+
+export default PageNav
---
+++ src/components/section-nav.js
@@ -0,0 +1,86 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+const SecNavContainer = styled.section`
+  padding: 2rem;
+  font-size: 1.4rem;
+  margin-top: 9rem;
+  h3 {
+    text-transform: uppercase;
+  }
+  ul {
+    padding: 0;
+    li {
+      list-style-type: none;
+      a:link,
+      a:hover,
+      a:active,
+      a:visited {
+        text-decoration: none;
+        padding: 1rem;
+        background-color: #eee;
+        color: #000;
+        display: block;
+        margin-bottom: 0.2rem;
+        border-radius: 0.4rem;
+        line-height: 1.3;
+      }
+      a:hover {
+        background-color: #dde;
+      }
+      a.active {
+        background-color: #333366;
+        color: #fff;
+        cursor: default;
+      }
+    }
+  }
+`
+
+const SectionNav = () => {
+  return (
+    <SecNavContainer>
+      <h3>Basic tutorial</h3>
+      <ul>
+        <li>
+          <Link to="/basic-introduction" activeClassName="active">
+            00 - Introduction
+          </Link>
+        </li>
+        <li>
+          <Link to="/basic-research" activeClassName="active">
+            01 - Research
+          </Link>
+        </li>
+        <li>
+          <Link to="/basic-conversation" activeClassName="active">
+            02 - Conversational fit?
+          </Link>
+        </li>
+        <li>
+          <Link to="/basic-flow" activeClassName="active">
+            03 - Flow design
+          </Link>
+        </li>
+        <li>
+          <Link to="/basic-design" activeClassName="active">
+            04 - Visual design
+          </Link>
+        </li>
+        <li>
+          <Link to="/basic-test" activeClassName="active">
+            05 - Testing
+          </Link>
+        </li>
+        <li>
+          <Link to="/basic-handoff" activeClassName="active">
+            06 - Handoff
+          </Link>
+        </li>
+      </ul>
+    </SecNavContainer>
+  )
+}
+
+export default SectionNav
