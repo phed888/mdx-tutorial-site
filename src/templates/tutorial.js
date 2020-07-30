396b02ee1bda904850da19cd7fc5c684a6710e13
Brian Speight <brspeight@expediagroup.com>
2020-07-29 17:46:22 -0700

    Removed 'ReadLink' import

--- src/templates/tutorial.js
+++ src/templates/tutorial.js
@@ -3,7 +3,6 @@ import { graphql } from "gatsby"
 import styled from "styled-components"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import DefaultLayout from "../layouts/defaultLayout"
-import ReadLink from "../styles/readlink"
 import PageNav from "../components/page-nav"
 
 const PageBodyContainer = styled.div`
