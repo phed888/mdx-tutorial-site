a1dc0577ea1f0be22010c90455f97125435dfd05
Brian Speight <brspeight@expediagroup.com>
2020-07-29 17:42:53 -0700

    Removed post listings and added generic overview

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,30 @@
 import React from "react"
-import { Link } from 'gatsby'
-import DefaultLayout from '../layouts/defaultLayout'
-import usePosts from '../hooks/usePosts'
-import PostPreview from '../components/post-preview'
+import DefaultLayout from "../layouts/defaultLayout"
 
 export default () => {
-
-  const posts = usePosts();
-  
   return (
-  <DefaultLayout>
-    <h1>Hello world!</h1>
-    <p>This is the home page</p>
-    <Link to="/test/">Test page</Link>
+    <DefaultLayout>
+      <h1>CDS design tutorials</h1>
+      <br />
+      <p>
+        The Conversation Design System (CDS) is a set of standards, components
+        and resources to facilitate the design of interactions with the Virtual
+        Agent Platform (VAP).
+      </p>
+      <p>
+        The tutorials on this site will walk through the process that the
+        Conversation Platform and Customer Experience (CPCE) have found to be
+        effective in the last 3 years of conversation design.
+      </p>
+      <p>
+        Use the navigation on the left to explore the parts of the tutorial.
+      </p>
+      {/* <Link to="/test/">Test page</Link> */}
 
-    <h2>Read my posts</h2>
+      {/* <h2>Read my posts</h2>
     {posts.map(post => (
       <PostPreview post={post} key={post.slug} />
-    ))}
-
-  </DefaultLayout>
-);
+    ))} */}
+    </DefaultLayout>
+  )
 }
