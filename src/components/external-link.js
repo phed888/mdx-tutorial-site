9bb32eec7efbf788a499e7f9d45f38913c86db74
Brian Speight <brspeight@expediagroup.com>
2020-08-03 17:47:36 -0700

    Added 'needed' flag to external link

--- src/components/external-link.js
+++ src/components/external-link.js
@@ -5,12 +5,25 @@ import styled from "styled-components"
 const ExternalLinkContainer = styled.div`
   display: inline-block;
   align-items: center;
+  position: relative;
   a:link,
   a:visited,
   a:hover,
   a:active {
     text-decoration: none;
     color: #0055ff;
+  }
+  &::after {
+    ${props => (props.needed ? `content: "Need content"` : null)};
+    white-space: nowrap;
+    color: #fff;
+    font-size: 1.2rem;
+    background-color: #f00;
+    padding: 0 0.8rem;
+    border-radius: 0.4rem;
+    position: absolute;
+    display: inline-block;
+    top: 0.7rem;
   }
   svg {
     transform: scale(0.75) translateY(0.8rem);
@@ -19,8 +32,8 @@ const ExternalLinkContainer = styled.div`
   }
 `
 
-const ExternalLink = ({ linkName, linkURL }) => (
-  <ExternalLinkContainer>
+const ExternalLink = ({ linkName, linkURL, needed }) => (
+  <ExternalLinkContainer needed={needed}>
     <a href={linkURL} target="_blank" rel="noreferrer">
       {linkName}
     </a>
