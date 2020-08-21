7278367797c9683559bc842efc65631be4c8efbc
Brian Speight <brspeight@expediagroup.com>
2020-08-20 17:37:36 -0700

    Adding the structure to index to match the posts.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,42 @@
 import React from "react"
 import DefaultLayout from "../layouts/defaultLayout"
 import PageFooter from "../components/page-footer"
+import styled from "styled-components"
+import PageNav from "../components/page-nav"
+
+const PageBodyContainer = styled.div`
+  display: flex;
+  .pageContent {
+    width: 90rem;
+  }
+`
 
 export default () => {
   return (
     <DefaultLayout>
-      <h1>CDS design tutorials</h1>
-      <br />
-      <p>
-        The Conversation Design System (CDS) is a set of standards, components
-        and resources to facilitate the design of interactions with the Virtual
-        Agent Platform (VAP).
-      </p>
-      <p>
-        The tutorials on this site will walk through the process that the
-        Conversation Platform and Customer Experience (CPCE) have found to be
-        effective in the last 3 years of conversation design.
-      </p>
-      <p>
-        Use the navigation on the left to explore the parts of the tutorial.
-      </p>
+      <PageBodyContainer>
+        <div class="pageContent">
+          <div>
+            <h1>CDS design tutorials</h1>
+            <br />
+            <p>
+              The Conversation Design System (CDS) is a set of standards,
+              components and resources to facilitate the design of interactions
+              with the Virtual Agent Platform (VAP).
+            </p>
+            <p>
+              The tutorials on this site will walk through the process that the
+              Conversation Platform and Customer Experience (CPCE) have found to
+              be effective in the last 3 years of conversation design.
+            </p>
+            <p>
+              Use the navigation on the left to explore the parts of the
+              tutorial.
+            </p>
+          </div>
+        </div>
+        <PageNav noHeader />
+      </PageBodyContainer>
       <PageFooter nextPage="00 - Introduction" nextSlug="basic-introduction" />
     </DefaultLayout>
   )
